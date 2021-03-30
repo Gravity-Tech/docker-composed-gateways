0e6aaa5f063a09b13484524c4eceb161c08d7169
Shamil Khalilov <shamfama@gmail.com>
2021-03-30 22:11:35 +0300

    Adjust extractors & bind nebulas script

--- bind-nebulas.sh
+++ bind-nebulas.sh
@@ -12,8 +12,27 @@ main() {
         shift
     done
 
+    echo "home is $home"
+
+    # ln -s "$home/nebulae/"
+
+    for file in $(ls ./nebulae)
+    do
+        sudo ln -fs "$PWD/nebulae/$file" "$home/nebulae/$file"
+    done
 
 
+    if [ -z "/home/extractor-configs/" ]
+    then
+        mkdir -p "/home/extractor-configs"
+    fi
+
+    for file in $(ls ./extractors)
+    do
+        sudo ln -fs "$PWD/extractors/$file" "/home/extractor-configs/$file"
+    done
+
+    docker-compose up -d
 }
 
 main $@
